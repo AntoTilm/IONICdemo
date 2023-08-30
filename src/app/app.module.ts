fb85a583c755a6ce06feafffd0874c3eda03acde
murgol75 <murgol@yahoo.fr>
2023-08-30 10:54:27 +0200

    passage en main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AddTaskPipe } from './add-task.pipe';
 
 @NgModule({
-  declarations: [AppComponent, AddTaskPipe],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
