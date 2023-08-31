5c852030920f8613ffb61a198f381571296ac755
Mazelt0f <murgol@yahoo.fr>
2023-08-31 16:28:20 +0200

    recup2

--- src/app/pages/add-task/add-task.page.ts
+++ src/app/pages/add-task/add-task.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TasksService } from 'src/app/shared/services/tasks.service';
 
 @Component({
@@ -10,7 +11,18 @@ import { TasksService } from 'src/app/shared/services/tasks.service';
 export class AddTaskPage implements OnInit {
   private _tasksService: TasksService; // Déclarer la propriété _tasksService
 
-  constructor(tasksService: TasksService, private formBuilder: FormBuilder) {
+  // constructor(private _fb : FormBuilder,
+  //   private _taskService : TasksService,
+  //   private _router : Router,
+  //   private _activeRoute : ActivatedRoute) { 
+  //     this.taskForm = this._fb.group({
+  //       name : [null],
+  //       assignedUser : [null],
+  //       deadline : [null],
+  //       status : [null],
+  //     });
+
+  constructor(tasksService: TasksService, private formBuilder: FormBuilder,private _router : Router,private _activeRoute : ActivatedRoute) {
     this._tasksService = tasksService; // Initialiser la propriété _tasksService avec l'instance reçue du constructeur
   }
 
@@ -29,5 +41,7 @@ export class AddTaskPage implements OnInit {
   onSubmitForm() {
     this._tasksService.tasks.push(this.taskForm.value);
     console.log(this._tasksService.tasks);
+this._router.navigateByUrl('task-list');
+
   }
 }
