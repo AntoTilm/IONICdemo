637c4870597710b624039b4813b6e891db3f8003
murgol75 <murgol@yahoo.fr>
2023-09-05 10:12:49 +0200

    modification de la liste de taches pré faite

--- src/app/shared/services/tasks.service.ts
+++ src/app/shared/services/tasks.service.ts
@@ -2,203 +2,192 @@ import { Injectable } from '@angular/core';
 import { iTask } from '../models/iTask';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
-
   tasks: iTask[] = [
     {
       id: 1,
       name: 'dishes washing',
       assignedUser: 'tof',
       deadline: new Date(2000, 8, 11),
-      status: 'in progress'
+      status: 'in progress',
     },
     {
       id: 2,
       name: 'dog walking',
       assignedUser: 'antoine',
       deadline: new Date(2025, 5, 8),
-      status: 'done'
+      status: 'done',
     },
     {
       id: 3,
       name: 'Hotdog Eating',
       assignedUser: 'Germain',
       deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      status: 'to start',
     },
     {
       id: 4,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Grocery shopping',
+      assignedUser: 'Alice',
+      deadline: new Date(2023, 9, 15),
+      status: 'done',
     },
+
     {
       id: 5,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Laundry',
+      assignedUser: 'Sophie',
+      deadline: new Date(2023, 9, 10),
+      status: 'done',
     },
+
     {
       id: 6,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Mow the lawn',
+      assignedUser: 'Bob',
+      deadline: new Date(2023, 9, 20),
+      status: 'in progress',
     },
+
     {
       id: 7,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Write report',
+      assignedUser: 'Emily',
+      deadline: new Date(2023, 9, 5),
+      status: 'to start',
     },
+
     {
       id: 8,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Paint the bedroom',
+      assignedUser: 'David',
+      deadline: new Date(2023, 9, 25),
+      status: 'in progress',
     },
+
     {
       id: 9,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Fix the car',
+      assignedUser: 'Michael',
+      deadline: new Date(2023, 9, 12),
+      status: 'done',
     },
+
     {
       id: 10,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Plan vacation',
+      assignedUser: 'Sarah',
+      deadline: new Date(2023, 9, 18),
+      status: 'done',
     },
+
     {
       id: 11,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Study for exam',
+      assignedUser: 'Rachel',
+      deadline: new Date(2023, 9, 8),
+      status: 'done',
     },
+
     {
       id: 12,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Attend meeting',
+      assignedUser: 'John',
+      deadline: new Date(2023, 9, 7),
+      status: 'in progress',
     },
+
     {
       id: 13,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Call the plumber',
+      assignedUser: 'Mark',
+      deadline: new Date(2023, 9, 14),
+      status: 'to start',
     },
+
     {
       id: 14,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Write blog post',
+      assignedUser: 'Laura',
+      deadline: new Date(2023, 9, 11),
+      status: 'in progress',
     },
+
     {
       id: 15,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Clean the garage',
+      assignedUser: 'Chris',
+      deadline: new Date(2023, 9, 22),
+      status: 'done',
     },
+
     {
       id: 16,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Organize the closet',
+      assignedUser: 'Olivia',
+      deadline: new Date(2023, 9, 9),
+      status: 'done',
     },
+
     {
       id: 17,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Water the plants',
+      assignedUser: 'Daniel',
+      deadline: new Date(2023, 9, 17),
+      status: 'in progress',
     },
     {
       id: 18,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Take out the trash',
+      assignedUser: 'Megan',
+      deadline: new Date(2023, 9, 13),
+      status: 'in progress',
     },
+
     {
       id: 19,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Walk the cat',
+      assignedUser: 'Ella',
+      deadline: new Date(2023, 9, 19),
+      status: 'to start',
     },
+
     {
       id: 20,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
-    },
-    {
-      id: 21,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
-    },
-    {
-      id: 22,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
-    },
-    {
-      id: 23,
-      name: 'Hotdog Eating',
-      assignedUser: 'Germain',
-      deadline: new Date(2023, 8, 30),
-      status: 'to start'
+      name: 'Bake a cake',
+      assignedUser: 'Liam',
+      deadline: new Date(2023, 9, 16),
+      status: 'done',
     },
   ];
 
-
-   constructor() {
-
-   }
+  constructor() {}
 
   getAll(): iTask[] {
-    return this.tasks
+    return this.tasks;
   }
 
   getById(id: number): iTask | undefined {
-    return this.tasks.find(iTask => iTask.id === id);
+    return this.tasks.find((iTask) => iTask.id === id);
   }
 
-  createTask(): void {
+  createTask(): void {}
 
-  }
-
-  updateTask(id:number, task : iTask): void {
-    let taskToUpdate : iTask | undefined = this.tasks.find(track => track.id === id);
+  updateTask(id: number, task: iTask): void {
+    let taskToUpdate: iTask | undefined = this.tasks.find(
+      (track) => track.id === id
+    );
     //Si track trouvée : modif
-    if(taskToUpdate) {
+    if (taskToUpdate) {
       taskToUpdate.name = task.name;
       taskToUpdate.assignedUser = task.assignedUser;
       taskToUpdate.deadline = task.deadline;
       taskToUpdate.status = task.status;
     }
   }
-  deleteTask(id : number): void { 
-    this.tasks = this.tasks.filter(tasks => tasks.id !== id);
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter((tasks) => tasks.id !== id);
   }
-
 }
